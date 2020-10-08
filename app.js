64d72858848b2f8dc2be29e2b9875a74e45ca91b
alejandro <meraza952@gmail.com>
2020-10-08 11:31:35 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const btnStartRecord = document.querySelector("#btnStartRecord");
+const btnStopRecord = document.querySelector("#btnStopRecord");
+const texto = document.querySelector("#texto");
+
+
+let recognition = new webkitSpeechRecognition();
+recognition.lang = 'es-ES';
+recognition.continuous = true;
+recognition.interimResults = false;
+
+recognition.onresult = (event)=>{
+    const results = event.results;
+    const frase = results[results.length - 1][0].transcript;
+    texto.value += frase;
+}
+
+btnStartRecord.addEventListener('click', ()=>{
+    recognition.start();
+});
+
+btnStopRecord.addEventListener('click', ()=>{
+    recognition.abort();
+})
