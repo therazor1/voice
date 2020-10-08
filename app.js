0b52eedc8cb0195fc4829223514747d186de84a2
alejandro <meraza952@gmail.com>
2020-10-08 11:36:39 -0500

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const texto = document.querySelector("#texto");
 let recognition = new webkitSpeechRecognition();
 recognition.lang = 'es-ES';
 recognition.continuous = true;
-recognition.interimResults = false;
+recognition.interimResults = true;
 
 recognition.onresult = (event)=>{
     const results = event.results;
